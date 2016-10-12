274c7c397c288d62aadce435ec430d17add1d727
Deshi Xiao <xiaods@gmail.com>
2016-10-12 19:04:04 +0800

    update base image to centos/7

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 # might not need sudo here if you run this as privileged in Vagrantfile
-sudo apt-get update
-sudo apt-get install git -y
-sudo apt-get install golang-go -y
-sudo apt-get install mercurial -y
+sudo yum update
+sudo yum install git -y
+sudo yum install go -y
+sudo yum install vim -y
 
 export PATH=$PATH:/usr/local/go/bin
 export GOPATH=/home/vagrant/dev
